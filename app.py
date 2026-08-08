2d80a5f7fff343bf29d1a6b1d8c3d40648f97590
Turabaev Azizbek <kbibi0865@gmail.com>
2026-08-08 19:29:22 +0500

    updated

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template, request
 import joblib
 import pandas as pd
 
-
 app = Flask(__name__)
 
+# Model va scalerni yuklash
 eng_yaxshi_model = joblib.load('sugurta_modeli.pkl')
 scaler = joblib.load('scaler.pkl')
-
 
 ustunlar_tartibi = [
     'age', 'bmi', 'children', 'is_female', 'is_smoker',
     'region_northwest', 'region_southeast', 'region_southwest',
     'bmi_category_Normal', 'bmi_category_Overweight', 'bmi_category_Obese'
 ]
-
-
 
 def sugurta_narxini_bashorat_qil(yosh, bmi, farzandlar_soni, ayolmi,
                                   chekadimi, hudud, model, scaler, ustunlar):
@@ -51,65 +48,65 @@ def sugurta_narxini_bashorat_qil(yosh, bmi, farzandlar_soni, ayolmi,
     return natija[0]
 
 
-@app.route('/')
-def hello():
-    return "hello world!"
+@app.route('/', methods=["GET"])
+def home():
+    return render_template('index.html', natija_usd=None)
+
 
 @app.route('/predict', methods=["GET", "POST"])
 def predict():
-    natija = None
+    if request.method == 'GET':
+        return render_template('index.html', natija_usd=None)
+
+    natija_usd = None
+    hisoblangan_bmi = None
+    bmi_holat = None
 
     if request.method == 'POST':
         yosh = request.form.get('yosh', type=float)
-        bmi = request.form.get('bmi', type=float)
+        boy_sm = request.form.get('height', type=float)
+        vazn_kg = request.form.get('weight', type=float)
         farzandlar_soni = request.form.get('children', type=int)
         ayolmi = request.form.get('gender') == '1'
         chekadimi = request.form.get('chekadimi') == '1'
         hudud = request.form.get('hudud')
 
-        natija = sugurta_narxini_bashorat_qil(
-            yosh=yosh,
-            bmi=bmi,
-            farzandlar_soni=farzandlar_soni,
-            ayolmi=ayolmi,
-            chekadimi=chekadimi,
-            hudud=hudud,
-            model=eng_yaxshi_model,
-            scaler=scaler,
-            ustunlar=ustunlar_tartibi
-        )
-        natija = round(float(natija), 2)
+        # BMI hisoblash
+        if boy_sm and vazn_kg and boy_sm > 0:
+            boy_m = boy_sm / 100.0
+            hisoblangan_bmi = vazn_kg / (boy_m ** 2)
 
-    return render_template('index.html', natija=natija)
+        if hisoblangan_bmi is not None:
+            if hisoblangan_bmi < 18.5:
+                bmi_holat = "Vazn yetarsiz (Underweight)"
+            elif hisoblangan_bmi < 25:
+                bmi_holat = "Me'yoriy / Sog'lom (Normal)"
+            elif hisoblangan_bmi < 30:
+                bmi_holat = "Ortiqcha vazn (Overweight)"
+            else:
+                bmi_holat = "Semizlik (Obese)"
 
+            natija_usd = sugurta_narxini_bashorat_qil(
+                yosh=yosh,
+                bmi=hisoblangan_bmi,
+                farzandlar_soni=farzandlar_soni,
+                ayolmi=ayolmi,
+                chekadimi=chekadimi,
+                hudud=hudud,
+                model=eng_yaxshi_model,
+                scaler=scaler,
+                ustunlar=ustunlar_tartibi
+            )
 
+            natija_usd = round(float(natija_usd), 2)
+            hisoblangan_bmi = round(hisoblangan_bmi, 1)
 
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#      if request.method == 'GET':
-#             return "Use a POST request with JSON data."
-
-    
-#      data = request.get_json() 
-       
-
-#      natija = sugurta_narxini_bashorat_qil(
-#             yosh=data['age'],
-#             bmi=data['bmi'],
-#             farzandlar_soni=data['children'],
-#             ayolmi=data['is_female'],
-#             chekadimi=data['is_smoker'],
-#             hudud=data['region'],
-#             model=eng_yaxshi_model,
-#             scaler=scaler,
-#             ustunlar=ustunlar_tartibi
-#         )
-
-#      return jsonify({"sugurta_narxi": float(natija)})
-
-   
-
+    return render_template(
+        'index.html',
+        natija_usd=f"{natija_usd:,.2f}".replace(',', ' ') if natija_usd else None,
+        bmi=hisoblangan_bmi,
+        bmi_holat=bmi_holat
+    )
 
 
 if __name__ == "__main__":
