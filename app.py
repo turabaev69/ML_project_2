784b11f2f8ec34d56029f302e9143268f0c90784
Turabaev Azizbek <kbibi0865@gmail.com>
2026-08-08 20:28:25 +0500

    b

--- app.py
+++ app.py
@@ -13,6 +13,58 @@ ustunlar_tartibi = [
     'bmi_category_Normal', 'bmi_category_Overweight', 'bmi_category_Obese'
 ]
 
+def sonni_soz_bilan_ifodalash(son):
+    if son == 0:
+        return "nol"
+
+    biraklar = ["", "bir", "ikki", "uch", "to'rt", "besh", "olti", "yetti", "sakkiz", "to'qqiz"]
+    onliklar = ["", "o'n", "yigirma", "o'ttiz", "qirq", "ellik", "oltmish", "yetmish", "sakson", "to'qson"]
+
+    def uchlikni_sozga_aylantirish(n):
+        natija = ""
+        yuzlik = n // 100
+        onlik = (n % 100) // 10
+        birak = n % 10
+
+        if yuzlik > 0:
+            if yuzlik == 1:
+                natija += "yuz "
+            else:
+                natija += biraklar[yuzlik] + " yuz "
+
+        if onlik > 0:
+            natija += onliklar[onlik] + " "
+
+        if birak > 0:
+            natija += biraklar[birak] + " "
+
+        return natija.strip()
+
+    if son < 0:
+        return "minus " + sonni_soz_bilan_ifodalash(-son)
+
+    daraja_nomlari = ["", "ming", "million", "milliard", "trillion"]
+    uchliklar = []
+    
+    temp = int(son)
+    while temp > 0:
+        uchliklar.append(temp % 1000)
+        temp //= 1000
+
+    sozlar = []
+    for i, uchlik in enumerate(uchliklar):
+        if uchlik > 0:
+            uchlik_matni = uchlikni_sozga_aylantirish(uchlik)
+            if i > 0:
+                if i == 1 and uchlik == 1:
+                    sozlar.append("ming")
+                else:
+                    sozlar.append(uchlik_matni + " " + daraja_nomlari[i])
+            else:
+                sozlar.append(uchlik_matni)
+
+    return " ".join(reversed(sozlar)).strip()
+
 def sugurta_narxini_bashorat_qil(yosh, bmi, farzandlar_soni, ayolmi,
                                   chekadimi, hudud, model, scaler, ustunlar):
     if bmi < 18.5:
@@ -46,19 +98,18 @@ def sugurta_narxini_bashorat_qil(yosh, bmi, farzandlar_soni, ayolmi,
     natija = model.predict(yangi_mijoz)
     return natija[0]
 
-
 @app.route('/', methods=["GET"])
 def home():
-    return render_template('index.html', natija_usd=None, natija_uzs=None)
-
+    return render_template('index.html', natija_usd=None, natija_uzs=None, natija_uzs_soz=None)
 
 @app.route('/predict', methods=["GET", "POST"])
 def predict():
     if request.method == 'GET':
-        return render_template('index.html', natija_usd=None, natija_uzs=None)
+        return render_template('index.html', natija_usd=None, natija_uzs=None, natija_uzs_soz=None)
 
     natija_usd = None
     natija_uzs = None
+    natija_uzs_soz = None
     hisoblangan_bmi = None
     bmi_holat = None
     yosh = None
@@ -110,12 +161,15 @@ def predict():
             summa_uzs = natija_usd * kurs_usd_uzs
             natija_uzs = round(summa_uzs, -3)
 
+            natija_uzs_soz = sonni_soz_bilan_ifodalash(int(natija_uzs))
+
             hisoblangan_bmi = round(hisoblangan_bmi, 1)
 
     return render_template(
         'index.html',
         natija_usd=f"{natija_usd:,.2f}".replace(',', ' ') if natija_usd else None,
         natija_uzs=f"{natija_uzs:,.0f}".replace(',', ' ') if natija_uzs else None,
+        natija_uzs_soz=natija_uzs_soz.capitalize() if natija_uzs_soz else None,
         bmi=hisoblangan_bmi,
         bmi_holat=bmi_holat,
         yosh=yosh,
@@ -126,7 +180,6 @@ def predict():
         chekadimi=chekadimi,
         hudud=hudud
     )
-
 
 if __name__ == "__main__":
     app.run(debug=True)
